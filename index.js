c2e73b9e8c6cf094debbcc8ee38f15a1b369252a
andypark-creator <Andypark0118@gmail.com>
2024-11-27 14:31:30 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log(`Hello`);
+console.log(`My name is Andy Park and I like to play Piano`);
+
+//window.alert(`tis is an alert!`);
+window.alert(`I like to eat pizza! `);
+
+document.getElementById("my H1").textContent = `Hello Everybody!`;
+document.getElementById("myP").textContent =  ` my name is Andy Park`;
+this.getcomputedS
